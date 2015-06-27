b8b1b5a65426db6c2ee3501b9f794fb1e7b3a463
Ayush Goel <ayushgoel111@gmail.com>
2015-06-27 19:21:07 +0530

    Add tags functionality to publish script

--- publish.sh
+++ publish.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
 
-# TODO: Add frontmatter.
-# TODO: Take tags.
-
 set -o errexit
 
 print_help () {
-    echo "Usage: ./publish.sh <path-to-post-file> <title-of-post>"
+    echo "Usage: ./publish.sh <path-to-post-file> <title-of-post> <tags>"
 }
 
 path=$1
 title=$2
+tags=${*:3}
 
-if test ! -f $path
+if test ! -f "$path"
 then
     print_help
     exit 1
 fi
 
-if test -z $title
+if test -z "$title"
 then
     print_help
     exit 1
 fi
 
-post_name=$(python lib/create-post/filename.py --title $title)
+post_name=$(python lib/create-post/filename.py --title "$title")
 post_path="_posts/$post_name"
+post_front_matter=$(python lib/create-post/front_matter.py --tags $tags)
 
-cp $path $post_path
-git add $post_path
+echo $"$post_front_matter" | cat - "$path" > temp
+mv temp $post_path
+
+git add "$post_path"
 git commit -m "Add post: $post_name"
 git push
